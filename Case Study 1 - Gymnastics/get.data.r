ebc2ffcbeaf33d31c968e8d63d464bcc2f6d2127
carlyleben <carly.benson@yale.edu>
2023-09-06 10:13:36 -0400

    pulling in data and outlining prep data

---
+++ Case Study 1 - Gymnastics/get.data.r
@@ -0,0 +1,5 @@
+## This script pulls down the raw gymnastics score files from GitHub.
+
+in_earlier_scores <- read.csv("gymnastics/data/data_2017_2021.csv")
+in_later_scores   <- read.csv("gymnastics/data/data_2022_2023.csv")
+
---
+++ Case Study 1 - Gymnastics/prep.data.r
@@ -0,0 +1,9 @@
+## This script cleans the raw gymnastics data in preparation for modeling.
+
+# Format column names
+
+# Format gymnast names, fix capitalization
+
+# Recode gender
+
+# Fix dates
