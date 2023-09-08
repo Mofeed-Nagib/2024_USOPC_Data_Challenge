821f5da602413480c9cc858ed174595e1238deba
Mofeed Nagib <mofeed.nagib@yale.edu>
2023-09-07 22:56:51 -0400

    Readd the files

---
+++ Case_Study_1-Gymnastics/get.data.r
@@ -0,0 +1,5 @@
+## This script pulls down the raw gymnastics score files from GitHub.
+
+in_earlier_scores <- read.csv("Case Study 1 - Gymnastics/data/data_2017_2021.csv")
+in_later_scores   <- read.csv("Case Study 1 - Gymnastics/data/data_2022_2023.csv")
+
---
+++ Case_Study_1-Gymnastics/prep.data.r
@@ -0,0 +1,12 @@
+## This script cleans the raw gymnastics data in preparation for modeling.
+
+# Create function to clean data
+  # Format column names
+  
+  # Format gymnast names, fix capitalization
+  
+  # Recode gender
+  
+  # Fix dates
+
+# Run function on both datasets
