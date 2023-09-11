0d0e3dc6558474b2a7f165b4933064976d9547be
carlyleben <carly.benson@yale.edu>
2023-09-11 19:58:17 -0400

    fit model notes

---
+++ Case_Study_1-Gymnastics/fit.model.r
@@ -0,0 +1,11 @@
+## This script predicts scores for each gymnast and apparatus.
+
+# For each gymnast (unique name)
+
+# For each apparatus
+
+# Grab that gymnast's earlier scores for that apparatus
+
+# Grab that gymnast's later scores for that apparatus
+
+
