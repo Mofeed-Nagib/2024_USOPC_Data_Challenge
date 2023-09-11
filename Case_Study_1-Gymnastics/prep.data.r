c8135b17ce4b245693d465d09e4bc0a16f806c7d
carlyleben <carly.benson@yale.edu>
2023-09-10 22:45:08 -0400

    cleaning up dates

--- Case_Study_1-Gymnastics/prep.data.r
+++ Case_Study_1-Gymnastics/prep.data.r
@@ -3,6 +3,10 @@ source("Case_Study_1-Gymnastics/get.data.r")
 
 in_earlier_scores
 in_later_scores
+
+# things to ask Brian:
+## how do we make a distribution using lack of data points
+
 # Create function to clean data
 clean_data <- function(in_data) {
   
@@ -17,10 +21,31 @@ clean_data <- function(in_data) {
                                   lastname  = str_to_title(lastname))
   
   # Fix dates
-  # get rid of day of the week
+  gym_data <- gym_data %>%
+    # get rid of days of the week
+    mutate(date = gsub("sun|thu", "", date, ignore.case = TRUE)) %>% 
+    
+    # handle ranges of dates
+    mutate(date = gsub(".*[-]", "", date)) %>% 
   
-  # handle ranges of dates
+    # fix capitalization, remove extra whitespace
+    mutate(date = str_trim(str_to_title(date))) %>% 
+    
+    # a few other adhoc fixes
+    mutate(date = gsub("July", "Jul", date)) %>% 
+    mutate(date = gsub(",", "", date)) %>% 
+    
+    # convert the month name into month number
+    mutate(date = strptime(gsub(" ", "-", date), format = "%d-%b-%Y")) %>% 
   
-  # make them Date types
+    # make them Date types
+    mutate(date = as.Date(date))
+  
+  if (any(is.na(gym_data$date))) {message("Warning: dates may not have processed properly. Some dates now NA.")}
+  
+  return(gym_data)
 }
+
 # Run function on both datasets
+earlier_scores <- clean_data(in_earlier_scores)
+later_scores   <- clean_data(in_later_scores)
