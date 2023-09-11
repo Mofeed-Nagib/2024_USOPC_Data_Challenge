1b22baa0aac5d38bdbaa827c17c906ff2c3a7e90
carlyleben <carly.benson@yale.edu>
2023-09-10 21:38:16 -0400

    test commit for janson

--- Case_Study_1-Gymnastics/prep.data.r
+++ Case_Study_1-Gymnastics/prep.data.r
@@ -2,13 +2,25 @@
 source("Case_Study_1-Gymnastics/get.data.r")
 
 in_earlier_scores
+in_later_scores
 # Create function to clean data
+clean_data <- function(in_data) {
+  
+  # Make working copy of data
+  gym_data <- in_data
+  
   # Format column names
+  colnames(gym_data) <- tolower(colnames(gym_data))
   
   # Format gymnast names, fix capitalization
-  
-  # Recode gender
+  gym_data <- gym_data %>% mutate(firstname = str_to_title(firstname),
+                                  lastname  = str_to_title(lastname))
   
   # Fix dates
-
+  # get rid of day of the week
+  
+  # handle ranges of dates
+  
+  # make them Date types
+}
 # Run function on both datasets
