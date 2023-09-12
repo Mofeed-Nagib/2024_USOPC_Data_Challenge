3f0926cce61e2ed0a55837e660c731e9a9f551cb
carlyleben <carly.benson@yale.edu>
2023-09-12 19:57:27 -0400

    adding some updated comments for team selection script

--- Case_Study_1-Gymnastics/select.team.R
+++ Case_Study_1-Gymnastics/select.team.R
@@ -1,9 +1,17 @@
 ## This script uses the distributions we created / scores we predicted in previous scripts
 ## to select the optimal gymnastics team!
 
-# Use distributions of earlier/later scores to simulate Olympic scores for all gymnasts
+# Run simulations
+# In each simulation:
 
-# Select athletes that maximize scores in other countries
+# Use distributions of earlier/later scores to simulate Olympic performances of other teams
+# Maybe factor in additional considerations: potential injury, experience level. etc??
 
-# Simulate how US teams perform relative to other countries, select US team with 'best' performance
-# where 'best' means best weighted medal count
+# Run through different possible US team constructions (is there a more efficient way than like looping through them? how to do this?)
+# Simulate US team performance
+
+# measure performance of US teams (by calculating a weighted medal count)
+
+# Keep track of which USA team construction performs best
+
+# At end of simulations, compare performances of different US team constructions
