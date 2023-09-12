ef00e2b70e64e1f7d7b419983dbea4a2d062168b
Mofeed Nagib <mofeed.nagib@yale.edu>
2023-09-11 20:37:43 -0400

    remove print

--- Case_Study_1-Gymnastics/fit.model.r
+++ Case_Study_1-Gymnastics/fit.model.r
@@ -3,7 +3,6 @@
 # For each gymnast (unique name)
 for (x in later_scores$fullname) {
   apparatus = unique(later_scores[later_scores$fullname == x, ]$apparatus)
-  print(apparatus)
   for (y in apparatus) {
     gymnast_apparatus_scores <- later_scores %>% filter(fullname == x & apparatus == y)
     
