885914d77551f8f4f4afe51dc72f17e75fd2f0d9
Mofeed Nagib <mofeed.nagib@yale.edu>
2023-09-11 23:39:06 -0400

    Build histogram models

--- Case_Study_1-Gymnastics/fit.model.r
+++ Case_Study_1-Gymnastics/fit.model.r
@@ -18,33 +18,43 @@ for (x in later_scores$fullname) {
     # get sample size of each gymnast + apparatus combo
     sample_size <- as.numeric(length(gymnast_apparatus_scores$score))
     
-    # take mean of scores
-    dist_mean <- mean(gymnast_apparatus_scores$score)
+    if (sample_size > 1) {
+      # take mean of scores
+      dist_mean <- mean(gymnast_apparatus_scores$score)
     
-    # calculate sd under CLT (?? validity of this -- consult with Brian)
-    dist_sd <- sd(gymnast_apparatus_scores$score) / sqrt(sample_size)
-    
-    # save mean and sd into list 
-    ls_gymnast_dist[[x]][[y]] <- c("fullname" = x, "apparatus" = y, "mean" = dist_mean, "sd" = dist_sd, "sample_size" = sample_size)
+      # calculate sd under CLT (?? validity of this -- consult with Brian)
+      dist_sd <- sd(gymnast_apparatus_scores$score) / sqrt(sample_size)
+      
+      # save mean and sd into list 
+      ls_gymnast_dist[[x]][[y]] <- c("fullname" = x, "apparatus" = y, "mean" = dist_mean, "sd" = dist_sd, "sample_size" = sample_size)
+    }
+    else {
+      # save mean and sd into list 
+      ls_gymnast_dist[[x]][[y]] <- c("fullname" = x, "apparatus" = y, "mean" = gymnast_apparatus_scores$score, "sd" = 0, "sample_size" = sample_size)
+    }
   }
 }
 
 # Make histograms of gymnast distribution by apparatus
-for (i in ls_gymnast_dist) {
-  # 
-  # so this is basically looping over each PERSON
-  # so if I want that person's info (setting i just as an example for testing line by line)
-  # i <- 7 
-  # ls_gymnast_dist[[i]][['FX]]['mean']
-  # so maybe for each person you want to loop over their apparatuses as well
-  # ls_gymnast_dist[[i]][[j]]['mean']
-  
-  # # Generate 10000 Gaussian deviates from mean and standard deviation
-  # data = rnorm(10000, mean = i["mean"], sd = i["sd"])
-  # 
-  # # Plot histogram with 40 bins
-  # hist(data, breaks=40, col="red", main=paste("Histogram of ", i['fullname'], "'s ", i['apparatus'], " Score Distribution"), col.main="blue")
+for (i in seq(1, length(ls_gymnast_dist))) {
+  for (j in seq(1, length(ls_gymnast_dist[[i]]))) {
+    # Generate 10000 Gaussian deviates from mean and standard deviation
+    data = rnorm(10000, mean = as.numeric(ls_gymnast_dist[[i]][[j]]['mean']), sd = as.numeric(ls_gymnast_dist[[i]][[j]]['sd']))
+
+    # Plot histogram with 40 bins
+    hist(data, breaks=40, col="red", main=paste("Histogram of", ls_gymnast_dist[[i]][[j]]['fullname'], "'s", ls_gymnast_dist[[i]][[j]]['apparatus'], "Score Distribution"), xlab = "Scores", col.main="blue")
+  }
 }
+
+
+
+# 
+# so this is basically looping over each PERSON
+# so if I want that person's info (setting i just as an example for testing line by line)
+# i <- 7 
+# ls_gymnast_dist[[i]][['FX]]['mean']
+# so maybe for each person you want to loop over their apparatuses as well
+# ls_gymnast_dist[[i]][[j]]['mean']
 
 
 # Grab that gymnast's earlier scores for that apparatus
