e56cd04f908ea52e06b30df0c6929f42d61ef5a6
carlyleben <carly.benson@yale.edu>
2023-09-11 22:44:58 -0400

    some comments on the for loop

---
+++ Case_Study_1-Gymnastics/select.team.R
@@ -0,0 +1,9 @@
+## This script uses the distributions we created / scores we predicted in previous scripts
+## to select the optimal gymnastics team!
+
+# Use distributions of earlier/later scores to simulate Olympic scores for all gymnasts
+
+# Select athletes that maximize scores in other countries
+
+# Simulate how US teams perform relative to other countries, select US team with 'best' performance
+# where 'best' means best weighted medal count
