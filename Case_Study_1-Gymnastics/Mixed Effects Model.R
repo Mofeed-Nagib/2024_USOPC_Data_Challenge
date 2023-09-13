01437ea03c2d0cbe62fa75205ef406aadf8fdd3e
jansonchu2001 <janjanchu2001@gmail.com>
2023-09-12 22:07:00 -0400

    new code

--- Case_Study_1-Gymnastics/Mixed Effects Model.R
+++ Case_Study_1-Gymnastics/Mixed Effects Model.R
@@ -1,29 +1,30 @@
+set.seed(1)
+
 d <- later_scores
 
-summary(model)
 
-set.seed(361)
-lm2 <- lm(score ~ apparatus + lastname + round, data = d)
-lmer2 <- lmer(score ~ (1 + apparatus|lastname) + round, data = d)
+lm2 <- lm(score ~ apparatus + fullname + round, data = d)
+lmer2 <- lmer(score ~ (1 + apparatus|fullname) + round, data = d)
 
 k=5
 folds=rep(1:k, length.out=nrow(d))
 d$fold = sample(folds, nrow(d), replace=F)
 
+d$lm1 <- NA
+d$lmer1 <- NA
 for(j in 1:k){
+  cat(j, "")
   train.rows <- d$fold!=j
   test.rows <- d$fold==j
   
-  lm1 <- lm(score ~ apparatus + lastname + round, data =d[train.rows,])
+  lm1 <- lm(score ~ apparatus + fullname + round, data =d[train.rows,])
   
-  lmer1<- lmer(score ~ (1 + apparatus|lastname) + round, data=d[train.rows,])
+  #lmer1<- lmer(score ~ (1 + apparatus|fullname) + round, data=d[train.rows,])
   
-  d$lm1[test.rows] <- predict(lm1, newdata=d[test.rows,])
-  d$lmer1[test.rows] <- predict(lmer1, newdata=d[test.rows,])
+  d[test.rows,]$lm1 <- predict(lm1, newdata=d[test.rows,], type='response', allow.new.levels=T)
+  #d[test.rows,]$lmer1 <- predict(lmer1, newdata=d[test.rows,], type='response', allow.new.levels=T)
 }
-
-d %>% dplyr::select(score, lastname, lm1, lmer1)
 
 sqrt(mean(d$lm1 - d$score)^2)
 
-sqrt(mean(d$lmer1 - d$score)^2)
+#sqrt(mean(d$lmer1 - d$score)^2)
