31fc927305d06fa4acc05bb7866460e1e5adcb7e
jansonchu2001 <janjanchu2001@gmail.com>
2023-09-18 09:54:29 -0400

    Mixed Effects based on Gender and Apparatus

--- Case_Study_1-Gymnastics/Mixed Effects Model.R
+++ Case_Study_1-Gymnastics/Mixed Effects Model.R
@@ -2,23 +2,264 @@ set.seed(1)
 
 d <- later_scores
 
-k=5
-folds=rep(1:k, length.out=nrow(d))
-d$fold = sample(folds, nrow(d), replace=F)
+# Men High Bar
+m_HB <- filter(d, gender=='m' & apparatus=='HB')
+  
 
-d$lm1 <- NA
-d$lmer1 <- NA
+k=5
+folds=rep(1:k, length.out=nrow(m_HB))
+m_HB$fold = sample(folds, nrow(m_HB), replace=F)
+
+m_HB$lm1 <- NA
+m_HB$lmer1 <- NA
 for(j in 1:k){
   cat(j, "")
-  train.rows <- d$fold!=j
-  test.rows <- d$fold==j
+  train.rows <- m_HB$fold!=j
+  test.rows <- m_HB$fold==j
   
   # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
-  lmer1<- lmer(score ~ (1 + apparatus|fullname) + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_HB[train.rows,])
   
   # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
-  d[test.rows,]$lmer1 <- predict(lmer1, newdata = d[test.rows,], type = 'response', allow.new.levels = T)
+  m_HB[test.rows,]$lmer1 <- predict(lmer1, newdata = m_HB[test.rows,], type = 'response', allow.new.levels = T)
 }
-
 # sqrt(mean(d$lm1 - d$score)^2)
-sqrt(mean(d$lmer1 - d$score)^2)
+# error = .033
+sqrt(mean(m_HB$lmer1 - m_HB$score)^2)
+
+
+# Men Pommel Horse
+
+m_PH <- filter(d, gender=='m' & apparatus=='PH')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(m_PH))
+m_PH$fold = sample(folds, nrow(m_PH), replace=F)
+
+m_PH$lm1 <- NA
+m_PH$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- m_PH$fold!=j
+  test.rows <- m_PH$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_PH[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  m_PH[test.rows,]$lmer1 <- predict(lmer1, newdata = m_PH[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .082
+sqrt(mean(m_PH$lmer1 - m_PH$score)^2)
+
+# Men Floor
+
+m_FX <- filter(d, gender=='m' & apparatus=='FX')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(m_FX))
+m_FX$fold = sample(folds, nrow(m_FX), replace=F)
+
+m_FX$lm1 <- NA
+m_FX$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- m_FX$fold!=j
+  test.rows <- m_FX$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_FX[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  m_FX[test.rows,]$lmer1 <- predict(lmer1, newdata = m_FX[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .011
+sqrt(mean(m_FX$lmer1 - m_FX$score)^2)
+
+# Men Parallel Bars
+
+m_PB <- filter(d, gender=='m' & apparatus=='PB')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(m_PB))
+m_PB$fold = sample(folds, nrow(m_PB), replace=F)
+
+m_PB$lm1 <- NA
+m_PB$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- m_PB$fold!=j
+  test.rows <- m_PB$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_PB[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  m_PB[test.rows,]$lmer1 <- predict(lmer1, newdata = m_PB[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .032
+sqrt(mean(m_PB$lmer1 - m_PB$score)^2)
+
+# Men Rings
+
+m_SR <- filter(d, gender=='m' & apparatus=='SR')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(m_SR))
+m_SR$fold = sample(folds, nrow(m_SR), replace=F)
+
+m_SR$lm1 <- NA
+m_SR$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- m_SR$fold!=j
+  test.rows <- m_SR$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_SR[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  m_SR[test.rows,]$lmer1 <- predict(lmer1, newdata = m_SR[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .065
+sqrt(mean(m_SR$lmer1 - m_SR$score)^2)
+
+# Men Vault
+
+m_VT <- filter(d, gender=='m' & apparatus=='VT')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(m_VT))
+m_VT$fold = sample(folds, nrow(m_VT), replace=F)
+
+m_VT$lm1 <- NA
+m_VT$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- m_VT$fold!=j
+  test.rows <- m_VT$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_VT[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  m_VT[test.rows,]$lmer1 <- predict(lmer1, newdata = m_VT[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .005
+sqrt(mean(m_VT$lmer1 - m_VT$score)^2)
+
+
+# Women Balance Beam
+
+w_BB <- filter(d, gender=='w' & apparatus=='BB')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(w_BB))
+w_BB$fold = sample(folds, nrow(w_BB), replace=F)
+
+w_BB$lm1 <- NA
+w_BB$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- w_BB$fold!=j
+  test.rows <- w_BB$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_BB[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  w_BB[test.rows,]$lmer1 <- predict(lmer1, newdata = w_BB[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .061
+sqrt(mean(w_BB$lmer1 - w_BB$score)^2)
+
+
+# Women Floor
+
+w_FX <- filter(d, gender=='w' & apparatus=='FX')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(w_FX))
+w_FX$fold = sample(folds, nrow(w_FX), replace=F)
+
+w_FX$lm1 <- NA
+w_FX$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- w_FX$fold!=j
+  test.rows <- w_FX$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_FX[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  w_FX[test.rows,]$lmer1 <- predict(lmer1, newdata = w_FX[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .064
+sqrt(mean(w_FX$lmer1 - w_FX$score)^2)
+
+# Women Unbalanced Beam
+
+w_UB <- filter(d, gender=='w' & apparatus=='UB')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(w_UB))
+w_UB$fold = sample(folds, nrow(w_UB), replace=F)
+
+w_UB$lm1 <- NA
+w_UB$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- w_UB$fold!=j
+  test.rows <- w_UB$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_UB[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  w_UB[test.rows,]$lmer1 <- predict(lmer1, newdata = w_UB[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .11
+sqrt(mean(w_UB$lmer1 - w_UB$score)^2)
+
+# Women Vault
+
+w_VT <- filter(d, gender=='w' & apparatus=='VT')
+
+
+k=5
+folds=rep(1:k, length.out=nrow(w_VT))
+w_VT$fold = sample(folds, nrow(w_VT), replace=F)
+
+w_VT$lm1 <- NA
+w_VT$lmer1 <- NA
+for(j in 1:k){
+  cat(j, "")
+  train.rows <- w_VT$fold!=j
+  test.rows <- w_VT$fold==j
+  
+  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+  lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_VT[train.rows,])
+  
+  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+  w_VT[test.rows,]$lmer1 <- predict(lmer1, newdata = w_VT[test.rows,], type = 'response', allow.new.levels = T)
+}
+# sqrt(mean(d$lm1 - d$score)^2)
+# error = .002
+sqrt(mean(w_VT$lmer1 - w_VT$score)^2)
