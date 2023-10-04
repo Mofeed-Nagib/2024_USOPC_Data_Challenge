bd742e0397bac47fcdb357e0361da11dbd5919d2
Mofeed Nagib <mofeed.nagib@yale.edu>
2023-10-04 10:07:31 -0400

    export mens files

--- Case_Study_1-Gymnastics/50_export_output.R
+++ Case_Study_1-Gymnastics/50_export_output.R
@@ -2,3 +2,6 @@
 
 saveRDS(df_female_us_teams, file = "Case_Study_1-Gymnastics/final_output/female_results.RDS")
 write_csv2(df_female_us_teams, file = "Case_Study_1-Gymnastics/final_output/female_results.csv")
+
+saveRDS(df_male_us_teams, file = "Case_Study_1-Gymnastics/final_output/male_results.RDS")
+write_csv2(df_male_us_teams, file = "Case_Study_1-Gymnastics/final_output/male_results.csv")
