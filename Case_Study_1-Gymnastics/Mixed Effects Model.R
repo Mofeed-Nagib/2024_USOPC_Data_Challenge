a8186fbbe30e8c4b73b4c1b72bb6e791f259d50f
jansonchu2001 <janjanchu2001@gmail.com>
2023-10-13 21:47:27 -0400

    commented code

--- Case_Study_1-Gymnastics/Mixed Effects Model.R
+++ Case_Study_1-Gymnastics/Mixed Effects Model.R
@@ -5,9 +5,12 @@ set.seed(12)
 d <- later_scores
 
 # Men High Bar
+#Doing CV using a lmer model for each apparatus and for each gender
+
 m_HB <- filter(d, gender=='m' & apparatus=='HB')
 m_HB$country <- factor(m_HB$country)  
 
+# doing 5 fold CV
 k=5
 folds=rep(1:k, length.out=nrow(m_HB))
 m_HB$fold = sample(folds, nrow(m_HB), replace=F)
@@ -15,6 +18,8 @@ m_HB$fold = sample(folds, nrow(m_HB), replace=F)
 
 m_HB$lmer1 <- NA
 m_HB$pred <- NA
+
+#Chose model as it had best AIC. Also chose lmer as we found that it had by far the lowest standard error compared to lm and using mean data for each player
 lmer1<- lmer(score ~ (1|fullname) + round, data = m_HB)
 for(j in 1:k){
   cat(j, "")
@@ -30,10 +35,10 @@ for(j in 1:k){
 
 m_HB_mean <- m_HB %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
 
-
+# error = 1.36
+# This is MSE
 sqrt(mean((m_HB$lmer1 - m_HB$score)^2))
-AIC_1 <- AIC(lmer1)
-print(AIC_1)
+
 # Men Pommel Horse
 
 m_PH <- filter(d, gender=='m' & apparatus=='PH')
@@ -44,7 +49,7 @@ folds=rep(1:k, length.out=nrow(m_PH))
 m_PH$fold = sample(folds, nrow(m_PH), replace=F)
 
 m_PH$lmer1 <- NA
-lmer2<- lmer(score ~ (1|fullname) + round + location, data = m_PH[train.rows,])
+
 for(j in 1:k){
   cat(j, "")
   train.rows <- m_PH$fold!=j
@@ -62,8 +67,6 @@ m_PH_mean <- m_PH %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
 sqrt(mean((m_PH$lmer1 - m_PH$score)^2))
 
 
-AIC_2 <- AIC(lmer2)
-print(AIC_2)
 # Men Floor
 
 m_FX <- filter(d, gender=='m' & apparatus=='FX')
@@ -80,13 +83,11 @@ for(j in 1:k){
   train.rows <- m_FX$fold!=j
   test.rows <- m_FX$fold==j
   
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_FX[train.rows,])
   
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
   m_FX[test.rows,]$lmer1 <- predict(lmer1, newdata = m_FX[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
+
 # error = 1.43
 sqrt(mean((m_FX$lmer1 - m_FX$score)^2))
 
@@ -107,13 +108,11 @@ for(j in 1:k){
   train.rows <- m_PB$fold!=j
   test.rows <- m_PB$fold==j
   
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_PB[train.rows,])
-  
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+
   m_PB[test.rows,]$lmer1 <- predict(lmer1, newdata = m_PB[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
+
 # error = 1.27
 sqrt(mean((m_PB$lmer1 - m_PB$score)^2))
 m_PB_mean <- m_PB %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
@@ -132,14 +131,12 @@ for(j in 1:k){
   cat(j, "")
   train.rows <- m_SR$fold!=j
   test.rows <- m_SR$fold==j
-  
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_SR[train.rows,])
-  
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+
   m_SR[test.rows,]$lmer1 <- predict(lmer1, newdata = m_SR[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
+
 # error = 1.28
 sqrt(mean((m_SR$lmer1 - m_SR$score)^2))
 m_SR_mean <- m_SR %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
@@ -158,14 +155,11 @@ for(j in 1:k){
   cat(j, "")
   train.rows <- m_VT$fold!=j
   test.rows <- m_VT$fold==j
-  
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = m_VT[train.rows,])
   
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
   m_VT[test.rows,]$lmer1 <- predict(lmer1, newdata = m_VT[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
 # error = 2.26
 sqrt(mean((m_VT$lmer1 - m_VT$score)^2))
 m_VT_mean <- m_VT %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
@@ -185,14 +179,12 @@ for(j in 1:k){
   cat(j, "")
   train.rows <- w_BB$fold!=j
   test.rows <- w_BB$fold==j
-  
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_BB[train.rows,])
   
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
   w_BB[test.rows,]$lmer1 <- predict(lmer1, newdata = w_BB[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
+
 # error = .99
 sqrt(mean((w_BB$lmer1 - w_BB$score)^2))
 w_BB_mean <- w_BB %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
@@ -212,17 +204,16 @@ for(j in 1:k){
   cat(j, "")
   train.rows <- w_FX$fold!=j
   test.rows <- w_FX$fold==j
-  
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_FX[train.rows,])
-  
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+
   w_FX[test.rows,]$lmer1 <- predict(lmer1, newdata = w_FX[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
-# error = .94
+
+#error = .93
 sqrt(mean((w_FX$lmer1 - w_FX$score)^2))
 w_FX_mean <- w_FX %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
+
 # Women Unbalanced Beam
 
 w_UB <- filter(d, gender=='w' & apparatus=='UB')
@@ -239,13 +230,11 @@ for(j in 1:k){
   train.rows <- w_UB$fold!=j
   test.rows <- w_UB$fold==j
   
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_UB[train.rows,])
   
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
   w_UB[test.rows,]$lmer1 <- predict(lmer1, newdata = w_UB[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
+
 # error = 1.17
 sqrt(mean((w_UB$lmer1 - w_UB$score)^2))
 w_UB_mean <- w_UB %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
@@ -265,14 +254,12 @@ for(j in 1:k){
   cat(j, "")
   train.rows <- w_VT$fold!=j
   test.rows <- w_VT$fold==j
-  
-  # lm1 <- lm(score ~ apparatus + fullname + round, data = d[train.rows,])
+
   lmer1<- lmer(score ~ (1|fullname) + round + location, data = w_VT[train.rows,])
-  
-  # d[test.rows,]$lm1 <- predict(lm1, newdata = d[test.rows,], type = 'response')
+
   w_VT[test.rows,]$lmer1 <- predict(lmer1, newdata = w_VT[test.rows,], type = 'response', allow.new.levels = T)
 }
-# sqrt(mean(d$lm1 - d$score)^2)
+
 # error = 1.88
 sqrt(mean((w_VT$lmer1 - w_VT$score)^2))
 w_VT_mean <- w_VT %>% group_by(fullname) %>% summarise(mean_lmer1 = mean(lmer1))
