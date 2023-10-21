66365903117346b75e9ace2afdb255b0141e60d7
Mofeed Nagib <mofeed.nagib@yale.edu>
2023-10-21 12:47:00 -0400

    Final updates

--- 50_export_output.R
+++ 50_export_output.R
@@ -1,9 +1,13 @@
 # Export output as RDS files
 
 saveRDS(df_female_us_teams, file = "final_output/female_results.RDS")
+saveRDS(df_female_us_teams, file = "gymnastics_app/data/female_results.RDS")
 write_csv2(df_female_us_teams, file = "final_output/female_results.csv")
+write_csv2(df_female_us_teams, file = "gymnastics_app/data/female_results.csv")
 saveRDS(out_female_medal_winners, file = "gymnastics_app/data/female_medals.RDS")
 
 saveRDS(df_male_us_teams, file = "final_output/male_results.RDS")
+saveRDS(df_male_us_teams, file = "gymnastics_app/data/male_results.RDS")
 write_csv2(df_male_us_teams, file = "final_output/male_results.csv")
+write_csv2(df_male_us_teams, file = "gymnastics_app/data/male_results.csv")
 saveRDS(out_male_medal_winners, file = "gymnastics_app/data/male_medals.RDS")
